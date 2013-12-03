0c7941edfaad8c0071042c3a21980fe36d8c72d3
eeallo <ealouh@gmail.com>
2013-12-03 13:37:53 +0200

    Update fix.js

--- fix.js
+++ fix.js
@@ -8,6 +8,6 @@ Version: 27.11.2013
 var $j = jQuery;
 
 $j(document).ready(function() {
-    $j('#wrapper').css('margin-bottom', -( $j('footer').height() ) );    
-    $j('#push').css('height', $j('footer').height() );
+    $j('.wrapper').css('margin-bottom', -( $j('.footer').height() ) );    
+    $j('.push').css('height', $j('.footer').height() );
 });
